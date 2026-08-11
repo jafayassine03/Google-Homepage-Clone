bea1cadb9c5c6f46b6af690a8c1148c2cab18912
Jafa Yassine <jafayassine03@gmail.com>
2026-08-11 23:10:30 +0800

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+ main (){
+     menu (){
+        Printf("/n 3zel chno baghi dir /n");
+        Printf("click 1 for +/n");
+        Printf("click 2 for -/n");
+        Printf("click 3 for / /n");
+        Printf("click 4 for */n");
+        Printf("click 5 to exit/n");
+     
+
+
+
+
+
+    }
+}
